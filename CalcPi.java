fd99c173a89d1b0052edecfb8dc1c9c1bd204919
Jeff Griffith <jgriffith@yahoo-inc.com>
2016-02-13 11:49:15 -0500

    fixed tabs

--- CalcPi.java
+++ CalcPi.java
@@ -13,65 +13,65 @@ import java.util.stream.Collectors;
  * PI/4 = 1/1 - 1/3 + 1/5 - 1/7 + 1/9 - ...
  */
 public class CalcPi {
-	
-	public static CompletableFuture<Double> calcPiForTerms(long start, long numElements) {
-		
-		CompletableFuture<Double> f = future();
-		
-		go(() -> {
-			double acc  = 0;
-			
-			for (long i = start; i < start + numElements; i++)
-				acc += 4.0 * (double)(1 - (i % 2) * 2) / (double)(2 * i + 1);
-			
-			f.complete(acc);
-		});
-		
-	    return f;
-	}
-	
-	public static CompletableFuture<Double> calculatePi(long numWorkers, long elementsPerWorker) {
-		
-		List<CompletableFuture<Double>> futures = list();
-		
-		for (int i = 0; i < numWorkers; ++i) {
-			CompletableFuture<Double> f = calcPiForTerms(i * elementsPerWorker, elementsPerWorker);
-			futures.add(f);
-		}
-		
-		return allOf(futures).thenApply(CalcPi::sum);
-	}
+    
+    public static CompletableFuture<Double> calcPiForTerms(long start, long numElements) {
+        
+        CompletableFuture<Double> f = future();
+        
+        go(() -> {
+            double acc  = 0;
+            
+            for (long i = start; i < start + numElements; i++)
+                acc += 4.0 * (double)(1 - (i % 2) * 2) / (double)(2 * i + 1);
+            
+            f.complete(acc);
+        });
+        
+        return f;
+    }
+    
+    public static CompletableFuture<Double> calculatePi(long numWorkers, long elementsPerWorker) {
+        
+        List<CompletableFuture<Double>> futures = list();
+        
+        for (int i = 0; i < numWorkers; ++i) {
+            CompletableFuture<Double> f = calcPiForTerms(i * elementsPerWorker, elementsPerWorker);
+            futures.add(f);
+        }
+        
+        return allOf(futures).thenApply(CalcPi::sum);
+    }
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-		long totalElements = 10000000000L;
-		long numWorkers = 8;
-		long elementsPerWorker = totalElements / numWorkers;
-		
-		printf("Calculating PI with %d workers and %d elements per worker\n", numWorkers, elementsPerWorker);
+        long totalElements = 10000000000L;
+        long numWorkers = 8;
+        long elementsPerWorker = totalElements / numWorkers;
+        
+        printf("Calculating PI with %d workers and %d elements per worker\n", numWorkers, elementsPerWorker);
 
-		Timer t = new Timer();
-		Double pi = calculatePi(numWorkers, elementsPerWorker).join();
-		t.stop();
-		
-		printf("Elapsed time: %dms\n", t.elapsed());
-		printf("Pi is %.10f\n", pi);
-	}
-	
-	///////////////// General stuff to make Java purdy.
-	
+        Timer t = new Timer();
+        Double pi = calculatePi(numWorkers, elementsPerWorker).join();
+        t.stop();
+        
+        printf("Elapsed time: %dms\n", t.elapsed());
+        printf("Pi is %.10f\n", pi);
+    }
+    
+    ///////////////// General stuff to make Java purdy.
+    
     public static <T> CompletableFuture<List<T>> allOf(List<CompletableFuture<T>> futures) {
-    	// Nurkiewitz.com
+        // Nurkiewitz.com
         CompletableFuture<Void> allDoneFuture = CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
         return allDoneFuture.thenApply(v -> futures.stream().map(future -> future.join()).collect(Collectors.<T> toList()));
     }
     
-	public static Double sum(List<Double> terms) {
-		double accum = 0.0;
-		for (Double t : terms)
-			accum += t;
-		return accum;
-	}
+    public static Double sum(List<Double> terms) {
+        double accum = 0.0;
+        for (Double t : terms)
+            accum += t;
+        return accum;
+    }
 
     @SafeVarargs
     public static <T> List<T> list(T... entries) {
@@ -79,7 +79,7 @@ public class CalcPi {
     }
     
     public static <T> CompletableFuture<T> future() {
-    	return new CompletableFuture<T>();
+        return new CompletableFuture<T>();
     }
     
     public static void go(Runnable task) {
@@ -87,19 +87,19 @@ public class CalcPi {
     }
     
     public static class Latch {
-    	private CountDownLatch cdl = new CountDownLatch(1);
-    	public void done() { cdl.countDown(); }
-    	public void await() throws InterruptedException { cdl.await(); }
+        private CountDownLatch cdl = new CountDownLatch(1);
+        public void done() { cdl.countDown(); }
+        public void await() throws InterruptedException { cdl.await(); }
     }
     
     public static class Timer {
-    	long startTime = System.currentTimeMillis();
-    	long elapsed;
-    	public void stop() { elapsed = System.currentTimeMillis() - startTime; }
-    	public long elapsed() { return elapsed; }
+        long startTime = System.currentTimeMillis();
+        long elapsed;
+        public void stop() { elapsed = System.currentTimeMillis() - startTime; }
+        public long elapsed() { return elapsed; }
     }
     
     public static void printf(String format, Object... args) {
-    	System.out.print(format(format, args));
+        System.out.print(format(format, args));
     }
 }
